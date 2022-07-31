626e96e486fede30988e6ac3c3980694f9aa01c8
j_ryadnova <accessyu@yandex.ru>
2022-07-31 18:38:27 +0300

    5 задание создан пакеты для разных зон ответственности.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
@@ -9,14 +9,14 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_contact(app):
+def test_add_group(app):
         app.open_home_page()
         app.login(username="admin",password="secret")
         app.open_contact_page()
         app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="",title="",company="",address="",home="",mobile="",work="",fax="",email="",bday="",bmonth="",byear="",address2="",phone2="",notes=""))
         app.logout()
 
-def test_add_empty_contact(app):
+def test_add_empty_group(app):
         app.open_home_page()
         app.login(firstname="", middlename="", lastname="", nickname="",title="",company="",address="",home="",mobile="",work="",fax="",email="",bday="",bmonth="",byear="",address2="",phone2="",notes="")
         app.open_contact_page()
