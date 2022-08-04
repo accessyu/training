a66fd7fdf843edc7dda415fafeb986ee5da530ce
j_ryadnova <accessyu@yandex.ru>
2022-08-04 19:39:38 +0300

    6 задание создана фикстура общая для всех тестов, рабочий.

--- fixture/session.py
+++ fixture/session.py
@@ -19,8 +19,3 @@ class SessionHelper:
             wd = self.app.wd
             wd.find_element_by_link_text("Logout").click()
 
-    def test_delete_first_group(app):
-        app.open_home_page()
-        app.session.login(username="admin", password="secret")
-        app.group.delete_first_group()
-        app.session.logout()
