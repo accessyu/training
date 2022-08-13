654b6ff446dd3c50510da2515065a7149cec134f
j_ryadnova <accessyu@yandex.ru>
2022-08-13 20:58:11 +0300

    10 задание

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,6 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
