7d9167830d813dd58f1bef737c405ea58002fe64
j_ryadnova <accessyu@yandex.ru>
2022-08-18 14:59:57 +0300

    15 задание

---
+++ test/15test_add_group.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from model.group import Group
+
+testdata = [
+        Group(name="123", header="1", footer="5"),
+        Group(name="", header="", footer="")
+]
+
+@pytest.mark.parametrize("group",testdata, ids=[])
+def test_add_group(app, group):
+    pass
+    # old_groups = app.group.get_group_list()
+    # app.group.create(group)
+    # assert len(old_groups) + 1 == app.group.count()
+    # new_groups = app.group.get_group_list()
+    # old_groups.append(group)
+    # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
