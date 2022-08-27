0ed7d48c5ed39a5c4b083cc6aaf3c3c2365d486b
j_ryadnova <accessyu@yandex.ru>
2022-08-27 20:41:58 +0300

    15 задание доработка от 27.08

--- test/15test_add_group.py
+++ test/15test_add_group.py
@@ -3,10 +3,12 @@ from model.group import Group
 import pytest
 import random
 import string
+import re
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    s= prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    return re.sub("\s+", " ",s).strip()
 
 testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
