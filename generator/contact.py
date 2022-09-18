dfb51287bb573460bb20a185da7e66108c82938d
j_ryadnova <accessyu@yandex.ru>
2022-09-18 22:07:57 +0300

    18 задание от 18.09 с правками.

---
+++ generator/contact.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+import re
+
+try:
+    opts,args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    s= prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    return re.sub("\s+", " ",s).strip()
+
+def random_contact():
+    return Contact(firstname=random_string("name", 10), middlename=random_string("name", 20),
+                      lastname=random_string("name", 10),
+                      nickname="4",
+                      title="11", company="12", address="23", home="44", mobile="5", work="7", fax="9",
+                      email="8", bday="2", bmonth="May", byear="1", address2="1", phone2="1", notes="1")
+
+testdata = [Contact(lastname="", firstname="",address="")] + [
+   random_contact() for _ in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,30 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
-import re
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    s= prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    return re.sub("\s+", " ",s).strip()
-
-def random_contact():
-    return Contact(firstname=random_string("name", 10), middlename=random_string("name", 20),
-                      lastname=random_string("name", 10),
-                      nickname="4",
-                      title="11", company="12", address="23", home="44", mobile="5", work="7", fax="9",
-                      email="8", bday="2", bmonth="May", byear="1", address2="1", phone2="1", notes="1")
-
-testdata = [Contact(lastname="", firstname="",address="")] + [
-   random_contact() for _ in range(5)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+        contact = data_contacts
         old_contacts = app.contact.get_contact_list()
-
         app.contact.create(contact)
         app.contact.return_to_contact()
         assert len(old_contacts) + 1 == app.contact.count
