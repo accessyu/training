3016bcbe29fb380be5187c83c9285a59c09834b2
j_ryadnova <accessyu@yandex.ru>
2022-11-02 15:45:27 +0300

    conftest доработка

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
-import importlib
-import pytest
-import json
-import jsonpickle
-import os.path
 from fixture.application import Application
 from fixture.db import DbFixture
-
+from fixture.orm import ORMFixture
+import jsonpickle
+import pytest
+import json
+import os.path
+import importlib
 
 fixture = None
 target = None
@@ -14,40 +14,40 @@ def load_config(file):
     global target
     if target is None:
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
-        with open(config_file) as config_file:
-            target = json.load(config_file)
+        with open(config_file) as active_file:
+            target = json.load(active_file)
     return target
 
 @pytest.fixture
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config( request.config.getoption("--target")) ['web']
-    """if target is None:
-       config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-       with open(config_file) as config_file:
-           target = json.load(config_file)"""
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
-@pytest.fixture (scope="session")
+@pytest.fixture(scope="session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'],
-                          user=db_config['user'], password=db_config['password'])
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
     def fin():
         dbfixture.destroy()
     request.addfinalizer(fin)
     return dbfixture
 
+@pytest.fixture(scope="session")
+def orm(request):
+    orm_config = load_config(request.config.getoption("--target"))['db']
+    ormfixture = ORMFixture(host=orm_config['host'], name=orm_config['name'], user=orm_config['user'], password=orm_config['password'])
+    return ormfixture
+
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        if fixture is not None:
-            fixture.session.ensure_logout()
-            fixture.destroy()
+        fixture.session.ensure_logout()
+        fixture.destroy()
     request.addfinalizer(fin)
     return fixture
 
@@ -61,18 +61,20 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
     parser.addoption("--check_ui", action="store_true")
 
+
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
-            testdate = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture,testdate, ids=[str(x) for x in testdate])
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
         elif fixture.startswith("json_"):
-            testdate = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdate, ids=[str(x) for x in testdate])
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(file):
-    with open (os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
